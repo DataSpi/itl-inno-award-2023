f5c176462d1f84a741e4f06e4a54960f5290a052
Pham Kiem <spinokiem@Phams-MacBook-Pro.local>
2023-07-20 15:01:50 +0700

    learned st about map plotting, but need to stick to the plan: building a chatbot

--- src/features/draft_testing_streamlit.py
+++ src/features/draft_testing_streamlit.py
@@ -1,21 +1,39 @@
 import streamlit as st
-
-st.title("Hello, I am Spino's first Streamlit Web App")
-st.header('This is header, but I dont know it is header 1 or 2 or 3')
-st.markdown("""
-> I can write what ever I want in this markdown block, & *this is awesome*
-            """)
-
-
 import numpy as np
 import pandas as pd
 
-map_data = pd.DataFrame(
-    np.random.randn(1000, 2) / [50, 50] + [37.76, -122.4],
-    columns=['lat', 'lon'])
 
-st.map(map_data)
+# st.title("Hello, I am Spino's first Streamlit Web App")
+# st.header('This is header, but I dont know it is header 1 or 2 or 3')
+# st.markdown("""
+# > I can write what ever I want in this markdown block, & *this is awesome*
+#             """)
 
-st.text_input("Your name", key="name") # user will put there name here. 
-# You can access the value at any point with: st.session_state.name
-st.text(f'hello {st.session_state.name}')
+# map_data = pd.DataFrame(
+#     np.random.randn(1000, 2) / [50, 50] + [37.76, -122.4],
+#     columns=['lat', 'lon'])
+
+# st.map(map_data)
+
+# st.text_input("Your name", key="name") # user will put there name here. 
+# # You can access the value at any point with: st.session_state.name
+# st.text(f'hello {st.session_state.name}')
+
+
+# vn_provinces = pd.read_html('https://www.latlong.net/category/cities-243-15.html')
+# vn_provinces = vn_provinces[0]
+# vn_provinces.rename(columns={'Latitude':'lat', 'Longitude':'lon'}, inplace=True)
+# st.dataframe(vn_provinces)
+# st.map(vn_provinces[['lat', 'lon']])
+
+import streamlit as st
+import pandas as pd
+
+# Highlight specific provinces
+highlighted_provinces = ['Hanoi', 'Ho Chi Minh City', 'Da Nang']
+
+# Create a DataFrame with province names and default latitude values
+df = pd.DataFrame({'province': highlighted_provinces, 'latitude': [0]*len(highlighted_provinces)})
+
+# Plot the map with highlighted provinces
+st.map(df, zoom=6, use_container_width=True, color='red')
